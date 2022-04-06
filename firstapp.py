306ef4ea66ede0174a54c746a6b241f39bd81aad
Kadatatlu Kishore <vskishore408@gmail.com>
2022-04-06 19:18:54 +0530

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return 'This is great !!!'
+    return 'This is great !!! Hurray '
 
 
 #Running...
